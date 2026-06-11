548eee19a6bc6f1c137b79fd50fe45d696d9050e
Conner Carraher <ccarraher@igsenergy.com>
2026-06-10 23:17:36 -0400

    Add cloudflare tunnel container to act as a reverse proxy and terminate TLS for web server

--- YTTheater.AppHost/AppHost.cs
+++ YTTheater.AppHost/AppHost.cs
@@ -3,24 +3,32 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddDockerComposeEnvironment("prod");
-
-var endpoint = builder.AddParameter("RegistryEndpoint");
-var repository = builder.AddParameter("RegistryRepository");
-var imageTag = Environment.GetEnvironmentVariable("IMAGE_TAG") ?? "dev";
-
-var registry = builder.AddContainerRegistry("DO", endpoint, repository);
-
 var server = builder.AddProject<Projects.YTTheater_Server>("server")
-    .WithHttpHealthCheck("/health")
-    .WithContainerRegistry(registry)
-    .WithImagePushOptions(x => x.Options.RemoteImageTag = imageTag)
-    .WithExternalHttpEndpoints();
+    .WithHttpHealthCheck("/health");
 
 var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
     .WithReference(server)
     .WaitFor(server);
 
-server.PublishWithContainerFiles(webfrontend, "wwwroot");
+if (builder.ExecutionContext.IsPublishMode)
+{
+    builder.AddDockerComposeEnvironment("prod");
+
+    var endpoint = builder.AddParameter("RegistryEndpoint");
+    var repository = builder.AddParameter("RegistryRepository");
+    var registry = builder.AddContainerRegistry("DO", endpoint, repository);
+    var imageTag = Environment.GetEnvironmentVariable("IMAGE_TAG") ?? "dev";
+
+    server
+        .WithContainerRegistry(registry)
+        .WithImagePushOptions(x => x.Options.RemoteImageTag = imageTag);
+
+    server.PublishWithContainerFiles(webfrontend, "wwwroot");
+
+    var tunnelToken = builder.AddParameter("CloudflaredTunnelToken", secret: true);
+    builder.AddContainer("cloudflared", "cloudflare/cloudflared", "latest")
+        .WithArgs("tunnel", "--no-autoupdate", "run", "--token", tunnelToken)
+        .WaitFor(server);
+}
 
 builder.Build().Run();
